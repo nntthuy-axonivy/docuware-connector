1c564698d448613f124486b43636ce196b15a6e1
peters-axon <pst@SATNBPST.localdomain>
2025-09-26 16:04:02 +0200

    MARP-2669 fixed JUnit tests in Maven Build

--- docuware-connector-test/src_test/com/axonivy/market/docuware/connector/test/DocuWareConnectorTest.java
+++ docuware-connector-test/src_test/com/axonivy/market/docuware/connector/test/DocuWareConnectorTest.java
@@ -1,19 +1,21 @@
 package com.axonivy.market.docuware.connector.test;
 
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 import com.axonivy.connector.docuware.connector.DocuWareFieldTableItem;
 import com.axonivy.connector.docuware.connector.DocuWareKeywordsField;
 import com.axonivy.connector.docuware.connector.DocuWareProperty;
-import com.axonivy.connector.docuware.connector.DocuWareService;
+import com.axonivy.connector.docuware.connector.auth.DocuWareAuthFeature;
 import com.axonivy.market.docuware.connector.test.BearerDisableFilter.BearerDisableFeature;
 
 import ch.ivyteam.ivy.application.IApplication;
 import ch.ivyteam.ivy.environment.AppFixture;
 import ch.ivyteam.ivy.environment.Ivy;
-import ch.ivyteam.ivy.rest.client.RestClient;
-import ch.ivyteam.ivy.rest.client.RestClients;
+import ch.ivyteam.ivy.request.EngineUriResolver;
+import ch.ivyteam.ivy.rest.client.mapper.JsonFeature;
 import ch.ivyteam.ivy.rest.client.security.CsrfHeaderFeature;
 import ch.ivyteam.ivy.scripting.objects.List;
 
@@ -33,58 +35,57 @@ public class DocuWareConnectorTest {
 	 */
 	@BeforeAll
 	public static void prepareRestClient(IApplication app, AppFixture fixture) {
-		var restClient = RestClients.of(app).find(DocuWareService.CLIENT_ID);
+		fixture.config("RestClients.DocuWare.Features", java.util.List.of(
+				JsonFeature.class.getCanonicalName(),
+				MultiPartFeature.class.getCanonicalName(),
+				DocuWareAuthFeature.class.getCanonicalName(),
+				BearerDisableFeature.class.getCanonicalName(),
+				CsrfHeaderFeature.class.getName()));
+	}
 
-		var builder = RestClient.create(restClient.name())
-				.uuid(restClient.uniqueId())
-				.uri(restClient.uri())
-				.description(restClient.description())
-				.properties(restClient.properties());
-
-		for (var feature : restClient.features()) {
-			builder.feature(feature.clazz());
-		}
-		builder.feature(BearerDisableFeature.class.getCanonicalName());
-		builder.feature(CsrfHeaderFeature.class.getCanonicalName());
-		restClient = builder.toRestClient();
-		RestClients.of(app).set(restClient);
+	@AfterAll
+	public static void showMBeanExceptionNotice() {
+		Ivy.log().warn("Note: Certain AxonIvy versions may log exceptions when registering the DocuWare REST Web Service MBean. These exceptions are harmless and can be ignored.");
 	}
 
 	@BeforeEach
-	public void prepareConfigurations(AppFixture fix) {
-		fix.var("docuwareConnector.test.url", "%s/api/docuWareMock".formatted(Ivy.html().applicationHomeLink().toAbsoluteUri().toString()));
+	public void prepareConfigurations(AppFixture fix, IApplication app) {
+		// The URL of the local DocuWare mock service.
+		var docuWareMockUrl = "%s/%s/api/docuWareMock".formatted(EngineUriResolver.instance().local(), app.getContextPath());
+
+		fix.var("docuwareConnector.test.url", docuWareMockUrl);
 		fix.var("docuwareConnector.test.grantType", "password");
 		fix.var("docuwareConnector.test.username", "testuser");
 		fix.var("docuwareConnector.test.password", "testpassword");
 
-		fix.var("docuwareConnector.passwordtest.url", "%s/api/docuWareMock".formatted(Ivy.html().applicationHomeLink().toAbsoluteUri().toString()));
+		fix.var("docuwareConnector.passwordtest.url", docuWareMockUrl);
 		fix.var("docuwareConnector.passwordtest.grantType", "password");
 		fix.var("docuwareConnector.passwordtest.username", "testuser");
 		fix.var("docuwareConnector.passwordtest.password", "testpassword");
 
-		fix.var("docuwareConnector.trustedtest1.url", "%s/api/docuWareMock".formatted(Ivy.html().applicationHomeLink().toAbsoluteUri().toString()));
+		fix.var("docuwareConnector.trustedtest1.url", docuWareMockUrl);
 		fix.var("docuwareConnector.trustedtest1.grantType", "trusted");
 		fix.var("docuwareConnector.trustedtest1.username", "testuser");
 		fix.var("docuwareConnector.trustedtest1.password", "testpassword");
 		fix.var("docuwareConnector.trustedtest1.impersonateUser", "^ivy:system=sysuser,anonymous=anonuser");
 
-		fix.var("docuwareConnector.trustedtest2.url", "%s/api/docuWareMock".formatted(Ivy.html().applicationHomeLink().toAbsoluteUri().toString()));
+		fix.var("docuwareConnector.trustedtest2.url", docuWareMockUrl);
 		fix.var("docuwareConnector.trustedtest2.grantType", "trusted");
 		fix.var("docuwareConnector.trustedtest2.username", "testuser");
 		fix.var("docuwareConnector.trustedtest2.password", "testpassword");
 		fix.var("docuwareConnector.trustedtest2.impersonateUser", "^session");
 
-		fix.var("docuwareConnector.trustedtest3.url", "%s/api/docuWareMock".formatted(Ivy.html().applicationHomeLink().toAbsoluteUri().toString()));
+		fix.var("docuwareConnector.trustedtest3.url", docuWareMockUrl);
 		fix.var("docuwareConnector.trustedtest3.grantType", "trusted");
 		fix.var("docuwareConnector.trustedtest3.username", "testuser");
 		fix.var("docuwareConnector.trustedtest3.password", "testpassword");
 		fix.var("docuwareConnector.trustedtest3.impersonateUser", "^session");
 
-		fix.var("docuwareConnector.dwtokentest1.url", "%s/api/docuWareMock".formatted(Ivy.html().applicationHomeLink().toAbsoluteUri().toString()));
+		fix.var("docuwareConnector.dwtokentest1.url", docuWareMockUrl);
 		fix.var("docuwareConnector.dwtokentest1.grantType", "dwtoken");
 		fix.var("docuwareConnector.dwtokentest1.dwToken", "^session");
 
-		fix.var("docuwareConnector.dwtokentest2.url", "%s/api/docuWareMock".formatted(Ivy.html().applicationHomeLink().toAbsoluteUri().toString()));
+		fix.var("docuwareConnector.dwtokentest2.url", docuWareMockUrl);
 		fix.var("docuwareConnector.dwtokentest2.grantType", "dwtoken");
 		fix.var("docuwareConnector.dwtokentest2.dwToken", "^session");
 	}
@@ -98,8 +99,9 @@ public class DocuWareConnectorTest {
 		propertyList.add(dwp);
 
 		DocuWareFieldTableItem dwt = new DocuWareFieldTableItem();
-		dwt.createRow().addColumnValue("NOTES__CONTENT", "HR input profile.", "String").addColumnValue("NOTES__AUTHOR",
-				"PTA", "String");
+		dwt.createRow()
+		.addColumnValue("NOTES__CONTENT", "HR input profile.", "String")
+		.addColumnValue("NOTES__AUTHOR", "PTA", "String");
 		DocuWareProperty dwtp = new DocuWareProperty("EMPLOYEE_NOTES", dwt, "Table");
 		propertyList.add(dwtp);
 
